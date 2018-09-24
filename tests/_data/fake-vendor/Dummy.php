d5ca159ba102584d3b5e1fe20d318660a4f6ea77
Tang Rufus <tangrufus@gmail.com>
2018-09-25 07:06:55 +0800

    Transformer: Test closures with `use` keywords

--- tests/_data/fake-vendor/Dummy.php
+++ tests/_data/fake-vendor/Dummy.php
@@ -14,6 +14,16 @@ use \UnexpectedValueException;
 use function OtherVendor\myFunc;
 use const OtherVendor\MY_MAGIC_NUMBER;
 
+$closure = function () use ($aaa) {
+    // Just testing.
+};
+
 class DummyClass
 {
+    public function useClosure()
+    {
+        array_map(function () use ($xxx) {
+            // Just testing.
+        }, []);
+    }
 }
