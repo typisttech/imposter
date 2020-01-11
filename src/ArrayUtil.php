b6cabdb9c4688b04453f2cc07140a5bea8addb5c
Tang Rufus <tangrufus@gmail.com>
2020-01-11 19:49:29 +0000

    `ArrayUtil`: Remove dead code

--- src/ArrayUtil.php
+++ src/ArrayUtil.php
@@ -22,10 +22,6 @@ class ArrayUtil
      */
     public static function flatten(array $array): array
     {
-        if (! is_array($array)) {
-            $array = (array) $array;
-        }
-
         $result = [];
         foreach ($array as $item) {
             if (is_array($item)) {
