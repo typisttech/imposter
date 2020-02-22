9d55968a68395f0e13e2fdee79283f40e4e4ca68
TangRufus <tangrufus@gmail.com>
2020-02-22 00:54:52 +0000

    Update Codeception

--- tests/_support/Helper/Unit.php
+++ tests/_support/Helper/Unit.php
@@ -6,7 +6,6 @@ namespace TypistTech\Imposter\Helper;
 
 use Codeception\Module;
 use Codeception\TestInterface;
-use Mockery;
 
 /**
  * Here you can define custom actions all public methods declared in helper class will be available in $I.
@@ -16,7 +15,6 @@ class Unit extends Module
     public function _after(TestInterface $test)
     {
         $this->getModule('Filesystem')->deleteDir(codecept_data_dir('tmp-vendor'));
-        Mockery::close();
     }
 
     public function _before(TestInterface $test)
